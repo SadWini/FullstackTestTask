362c769afbc6f4c939e591ad8d13dca0f06f3d0b
SadWini <skylorun@gmail.com>
2024-11-02 15:26:25 +0300

    Add base structure

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Order
+{
+    public int Id { get; set; }
+       
+    [Required]
+    public string SenderCity { get; set; }
+       
+    [Required]
+    public string SenderAddress { get; set; }
+       
+    [Required]
+    public string RecipientCity { get; set; }
+       
+    [Required]
+    public string RecipientAddress { get; set; }
+       
+    [Required]
+    public double Weight { get; set; }
+
+    [Required]
+    public DateTime PickupDate { get; set; }
+
+    public string OrderNumber => $"ORD-{Id:D5}";
+}
